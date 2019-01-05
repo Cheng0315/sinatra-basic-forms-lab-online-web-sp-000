689cf496215484fa4cf80c359cf710276db05b5d
Cheng Vue <cvu3@ucsc.edu>
2019-01-05 09:21:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,9 +11,9 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    params(:name)
-    params(:breed)
-    params(:age)
+    params[:name]
+    params[:breed]
+    params[:age].to_i
 
     erb :display_puppy
   end
